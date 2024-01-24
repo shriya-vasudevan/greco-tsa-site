1a2fd9b6c4a59cd7312f32b01f1525ebdb6e0622
Shriya Vasudevan <157411976+shriya-vasudevan@users.noreply.github.com>
2024-01-24 18:22:15 +0000

    Remove type module

--- scripts/huggingface.mjs
+++ scripts/huggingface.mjs
@@ -5,7 +5,7 @@ for await (
     accessToken: "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",
     model: "HuggingFaceH4/zephyr-7b-beta",
     inputs: "Hello there!",
-    parameters: { max_length: 100 }
+    // parameters: { max_length: 100 }
   })) {
     
   if (output.generated_text != null) { console.log(output.generated_text) };

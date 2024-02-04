8dcc581845f74afdf7746e283ca3f7684718f243
S. M. V. <117322465+SMVthe1st@users.noreply.github.com>
2024-02-04 23:23:31 +0000

    Delete unnecessary files, minor edits

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home () {
   return (
     <main>
       <h1 className="text-green-50">GRECO</h1>
+      <Image src="/circlez.svg" width="546" height="832" alt=""/>
       <Image src="/windmill.svg" width="389" height="759" alt=""/>
     </main>
   );

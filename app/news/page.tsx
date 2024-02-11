264d884d33d2c8cd4c27bd5ef8faebd7d863a263
S. M. V. <117322465+SMVthe1st@users.noreply.github.com>
2024-02-11 17:17:57 +0000

    Cleaner

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,20 +1,12 @@
 "use server"
 
 import "dotenv/config";
-import Link from "next/link";
 
 async function news (): Promise<any> {
     const req = await fetch(`https://newsapi.org/v2/everything?q='green energy'&apiKey=${process.env.NEWS_API_KEY}`);
     const resp = await req.json();
     return resp;
 };
-
-/* news().then(resp => {
-    resp.articles.map((i: { [key: string]: string; }) => {
-        console.log(typeof(i.urlToImage));
-        console.log(i.urlToImage);
-    });
-}); */
 
 export default async function News () {
     const data = await news();

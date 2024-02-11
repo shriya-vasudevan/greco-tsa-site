143de21ba85a3866d192a79c94c711076dc4e280
S. M. V. <117322465+SMVthe1st@users.noreply.github.com>
2024-02-11 17:14:54 +0000

    Working news API

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,7 +1,7 @@
 "use server"
 
 import "dotenv/config";
-import Image from "next/image";
+import Link from "next/link";
 
 async function news (): Promise<any> {
     const req = await fetch(`https://newsapi.org/v2/everything?q='green energy'&apiKey=${process.env.NEWS_API_KEY}`);
@@ -9,15 +9,12 @@ async function news (): Promise<any> {
     return resp;
 };
 
-news().then(resp => {
+/* news().then(resp => {
     resp.articles.map((i: { [key: string]: string; }) => {
-        // console.log(i.title);
         console.log(typeof(i.urlToImage));
         console.log(i.urlToImage);
     });
-});
-
-// line 30 --> <Image href={i.urlToImage} width={42.5} height={42.5} />
+}); */
 
 export default async function News () {
     const data = await news();
@@ -25,14 +22,16 @@ export default async function News () {
     return (
         <>
             <h1>Latest News</h1>
-             <div>
+             <div className="">
                 {data.articles.map((i: { [key: string]: string; }) => {
                     return (
                         <div>
-                            <img src={i.urlToImage} width="42.5%"></img>
-                            <h2>{i.title}</h2>
-                            <p>{i.publishedAt}</p>
-                            <p>{i.description}</p>
+                            <a href={i.url}>
+                                <img src={i.urlToImage}></img>
+                                <h2>{i.title}</h2>
+                                <p>{i.publishedAt}</p>
+                                <p>{i.description}</p>
+                            </a>
                         </div>
                     );
                 })}

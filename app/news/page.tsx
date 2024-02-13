07aa861d11d145638c45cb1e195172c3166fe7f2
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2024-02-12 21:05:09 -0500

    Update page.tsx

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,19 +1,36 @@
 // "use server"
 
-// import "dotenv/config";
+import "dotenv/config";
 
-/* async function news () {
+async function news () {
     const req = await fetch(`https://newsapi.org/v2/everything?q='green energy'&apiKey=${process.env.NEWS_API_KEY}`);
     const resp = await req.json();
     return resp;
-}; */
+}; 
 
 export default /* async */ function News () {
-    // const data = await news();
+    const data = await news();
 
     return (
         <>
             <h1>Latest News</h1>
+            <div className="">
+                {data.articles ? (
+                    data.articles.map((i: { title: string, url: string, urlToImage: string, description: string, publishedAt: string }) => {
+                        return (
+                            <div key={i.title}>                        
+                                <a href={i.url}>
+                                    <img src={i.urlToImage} alt={i.description}></img>
+                                    <h2>{i.title}</h2>
+                                    <p>{i.publishedAt}</p>
+                                    <p>{i.description}</p>
+                                </a>
+                            </div>
+                        );
+                })) : (
+                    <p>Loading...</p>
+                )}
+            </div>
             {/* <div className="">
                 {data.articles?.map((i: { title: string, url: string, urlToImage: string, description: string, publishedAt: string }) => {
                     return (

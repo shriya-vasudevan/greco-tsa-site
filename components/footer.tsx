e4f953d9dae3c08a78523b74d99d7f2481156cb1
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2024-02-14 00:25:48 +0000

    Pending changes exported from your codespace

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,23 @@
 import { NAV_LINKS } from "@/scripts/constants";
+import Link from "next/link";
 
 const Footer = () => {
     return(
-        <footer></footer>
+        <footer className="relative w-full">
+            <div className="mx-auto w-full max-w-7xl px-8">
+                <div>
+                    {NAV_LINKS.map((a) => (
+                        <ul className="space-y-1">
+                            <li>
+                                <Link key={a.label} href={a.href}>
+                                    {a.label}
+                                </Link>
+                            </li>
+                        </ul>
+                    ))}
+                </div>
+            </div>
+        </footer>
     );
 };
 

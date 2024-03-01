8e3e5c9104df1cc1df01681e04642f8ce03eed8d
Shriya Vasudevan <157411976+shriya-vasudevan@users.noreply.github.com>
2024-03-01 17:23:47 +0000

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Title from "@/components/title";
 import { inter, lato } from '@/app/ui/fonts';
 
 export default function Home () {
   return (
     <main>
-      <h1 className="text-green-50">GRECO</h1>
+      <Title title="GRECO"/>
       <Image src="/circlez.svg" width="546" height="832" alt=""/>
       <Image src="/windmill.svg" width="389" height="759" alt=""/>
     </main>

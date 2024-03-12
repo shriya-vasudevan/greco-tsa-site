dea63e72753d218e96d941f2f12c053d07436cfa
Shriya Vasudevan <157411976+shriya-vasudevan@users.noreply.github.com>
2024-03-12 22:03:26 +0000

    source links + carousel content

--- scripts/constants.tsx
+++ scripts/constants.tsx
@@ -7,3 +7,22 @@ export const NAV_LINKS = [
     { href: "/news", label: "News" }
 ];
 
+export const SOURCE_LINKS = [
+    { href: "https://www.eia.gov/", label: "EIA"},
+    { href: "https://www.iea.org/", label: "IEA"},
+    { href: "https://www.weforum.org/", label: "WEF"},
+    { href: "https://www.epa.gov/", label: "EPA"},
+    { href: "https://www.irs.gov/", label: "IRS"},
+    { href: "https://www.nationalgrid.com/", label: "National Grid"},
+    { href: "https://www.energy.gov/", label: "Department of Energy"},
+    { href: "https://www.nrel.gov/", label: "NREL"},
+    { href: "https://www.energystar.gov/", label: "EnergyStar"},
+];
+
+export const CAROUSEL_CONTENT = [
+    { title: "SOLAR", image: "/carousel/solar.jpg", content: `Solar energy works by converting energy from the sun into power. There are two types of solar energy that can be used by homeowners: heat and electricity. Solar PV panels generate electricity, while solar thermal panels generate heat; installing solar PV panels helps offset most home energy needs, while solar thermal panels allow you to offset home heating energy.\n\nAverage cost: $38,000 for a 10 kW system\nAverage time to install: 2 days`},
+    { title: "WIND", image: "/carousel/wind.jpg", content: "Wind energy is created using a wind turbine, which channels wind power to create electricity. The blades of a turbine are attached to a rotor that spins to generate electricity. Before installing a small home wind system, you must keep in mind the amount of wind you receive as well as zoning requirements. For the average home using ~10,650 kW annually, a wind turbine in the range of 5-15 kW will be needed to make a significant contribution.\n\nAverage cost: $60,000 for a 10 kW system\nAverage time to install: Varies"},
+    { title: "GEOTHERMAL", image: "/carousel/geothermal.webp", content: "Geothermal energy is heat energy from the Earth from hot water underground. For homeowners, geothermal heat pumps are the most common method of harnessing geothermal energy and can heat/cool the house as well as supply it with hot water, if so equipped. There are 4 types of geothermal pump systems: horizontal, vertical, pond/lake, and open-loop. For residential homes, a horizontal pumping system works best and is most cost-effective. Before installation, you must factor in how much land you have.\n\nAverage cost: $25,000 for horizontal pumps\nAverage time to install: 2 days"},
+    { title: "HYDROELECTRIC", image: "/carousel/hydroelectric.jpeg", content: "Hydroelectric power uses a dam to alter the flow of a river or other body of water. The water is then used to spin the blades of a turbine, generating electricity. If you have water flowing through your property, you might consider building a small hydropower - micro-hydropower -system. A micro-hydropower system can provide enough power for all home electricity needs and is a more affordable method of green energy.\n\nAverage cost: $20,000 for a 10 kW system\nAverage time to install: Varies"},
+    { title: "BIOMASS", image: "/carousel/biomass.png", content: "Biomass is renewable organic material from animals and plants. It can be directly burnt to produce heat, converted to produce fuel, or used to generate electricity through steam turbines. Decomposing biomass emits a methane-rich product called bio-gas, which can even be used in stoves to cook food; biomass stoves/boilers are a popular option for homeowners.\n\nAverage cost: $21,000 for stove + boiler\nAverage time to install: 1 day"},
+];

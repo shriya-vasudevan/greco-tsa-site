025f3361fc7db2484a6c475a0137341689f4d40a
Shriya Vasudevan <157411976+shriya-vasudevan@users.noreply.github.com>
2024-03-12 22:03:07 +0000

    news

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -15,14 +15,8 @@ export default async function News () {
     return (
         <>
             <h1 className="mt-10 pl-8 h1 border-green-50 border-l-4">Latest News</h1>
-            {/* <div className="p-8">
-            {
-                <NewsCard key={data.articles[0].title} url={data.articles[0].url} title={data.articles[0].title} image={data.articles[0].urlToImage} published={data.articles[0].publishedAt.slice(0, 10)} description={data.articles[0].description}/>
-            }
-            </div> */}
-            <div className="relative grid grid-flow-row auto-rows-max md:grid-cols-2 items-end justify-center overflow-hidden p-8">
+            <div className="relative grid grid-flow-row auto-rows-max lg:grid-cols-2 items-end justify-center overflow-hidden p-8">
                 {data.articles ? (
-                    // d = data.articles.slice(0)
                     data.articles.map((i: { title: string, url: string, urlToImage: string, description: string, publishedAt: string }) => {
                         if (i.urlToImage && i.urlToImage != "https://cdn.vox-cdn.com/thumbor/h5kNb06_KICKtrH3Z6uuDM5gxG4=/0x0:4000x2656/1200x628/filters:focal(2000x1328:2001x1329)/cdn.vox-cdn.com/uploads/chorus_asset/file/25283603/566448831.jpg" && i.urlToImage != "https://i.abcnewsfe.com/a/3be4cffc-d6ea-47eb-90c8-cbc0d6b31c11/wirestory_a3d63c87a6f59a66f7f103f325214f64_16x9.jpg?w=1600") {
                             return (   

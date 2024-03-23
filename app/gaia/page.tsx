ad2e8f3519ddc67bf398df401d7694977c01d2a3
Shriya Vasudevan <157411976+shriya-vasudevan@users.noreply.github.com>
2024-03-23 18:26:56 +0000

    SMART AI OMG OMG OMG AYYYYYYY

--- app/gaia/page.tsx
+++ app/gaia/page.tsx
@@ -2,12 +2,25 @@
 
 import "dotenv/config";
 import { useState } from "react";
-import { HfInference } from "@huggingface/inference";
-import { Tooltip, Button } from "@material-tailwind/react";
+import { Tooltip } from "@material-tailwind/react";
 import Image from "next/image";
 import { FaArrowRight } from "react-icons/fa";
 
-const hf = new HfInference(process.env.HF_API_KEY);
+async function session () {
+  const req = await fetch("https://api.inworld.ai/v1/workspaces/default-dszrtuet_4tvehuoopv_1q/characters/gaia:openSession", {
+      method: "POST",
+      headers: {
+          "content-type": "application/json",
+          "authorization": "Basic NXFqbWN0aTlhdDdVVGdESzZscXBxZ2c2Ump5Qkk5OTI6cVQ4alRZbWwzUUNjRVEyT2RhT3RxNXJKMHAxVXdvZmpIcXF6Q3d3TkJRR3AySUxjSmtIM25WTlNFOXVxSGN6aw=="
+      },
+      body: JSON.stringify({
+          "character": "workspaces/default-dszrtuet_4tvehuoopv_1q/characters/galadriel",
+          "user": {"givenName": "user"}
+      })
+  });
+  const resp = await req.json();
+  return resp;
+};
 
 export default function GAIA () {
   const [message, setMessage] = useState("");
@@ -23,19 +36,27 @@ export default function GAIA () {
     setChats(chats);
     setMessage("");
 
-    await hf.textGeneration({
-      model: "HuggingFaceH4/zephyr-7b-beta",
-      inputs: message
-    })      
-    .then((output) => {
-      chats.push(output.generated_text);
-      setChats(chats);
-      setIsTyping(false);
-    })
-    .catch((error) => {
-      console.log(error);
-      alert(error);
+    const data = await session();
+    const req = await fetch(`https://api.inworld.ai/v1/workspaces/default-dszrtuet_4tvehuoopv_1q/sessions/${data.name}/sessionCharacters/${data.sessionCharacters[0].character}:sendText`, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "authorization": "Basic NXFqbWN0aTlhdDdVVGdESzZscXBxZ2c2Ump5Qkk5OTI6cVQ4alRZbWwzUUNjRVEyT2RhT3RxNXJKMHAxVXdvZmpIcXF6Q3d3TkJRR3AySUxjSmtIM25WTlNFOXVxSGN6aw==",
+            "Grpc-Metadata-session-id": `${data.name}`
+        },
+        body: JSON.stringify({
+            "text": message
+        })
     });
+
+    const resp = await req.json();
+    const output = resp.textList;
+
+    chats.push(output);
+    setChats(chats);
+    setIsTyping(false);
+
+    return output;
   };
 
   return (
@@ -47,7 +68,7 @@ export default function GAIA () {
         <div className="items-center bg-transparent border-green-50 border-l-2 border-t-2 h-5/6 w-2/4 p-0"></div>
         <div className="items-center bg-transparent border-green-50 border-r-2 border-t-2 h-5/6 w-2/4 p-0"></div>
       </div>
-      <h2 className="relative px-4 text-lg font-medium text-center m-0">Hi there, I&apos;m<span> </span>
+      <h2 className="relative px-4 text-lg font-bold text-center m-0">Hi there, I&apos;m<span> </span>
         <span className="text-green-50 cursor-pointer">
           <Tooltip className="bg-black-50 text-white-50 p-8 w-96 text-center" content="Geo Artificial Intelligence Assistant (GAIA) is an AI model built to help homeowners find economic green energy solutions. However, GAIA can also be used for... More fun purposes!" placement="top">
             GAIA
@@ -64,8 +85,8 @@ export default function GAIA () {
         ) : <p className="text-center w-10/12">{chats[chats.length - 1]}</p>}
       </div>
       <div className="relative flex flex-col gap-8 items-center px-4 pb-10 w-screen">      
-        <button className="bg-white-50/30 p-4 rounded-full w-80 md:w-96 text-base text-white-50" onClick={(e) => chat(e, "Cheap green energy options for homeowners")}>Cheap green energy options for homeowners</button>
-        <button className="bg-white-50/30 p-4 rounded-full w-80 md:w-96 text-base text-white-50" onClick={(e) => chat(e, "Tell me a random joke!")}>Tell me a random joke!</button>
+        <button className="bg-white-50/20 p-4 rounded-full w-80 md:w-96 text-base text-white-50" onClick={(e) => chat(e, "Cheap green energy options for homeowners")}>Cheap green energy options for homeowners</button>
+        <button className="bg-white-50/20 p-4 rounded-full w-80 md:w-96 text-base text-white-50" onClick={(e) => chat(e, "Tell me a random joke!")}>Tell me a random joke!</button>
       </div>
       <div className="flex flex-cols justify-center pt-10 px-4 h-48">
         <form className="relative flex text-center min-w-80 md:min-w-96 w-2/4" action="" onSubmit={(e) => chat(e, message)}>
@@ -75,14 +96,13 @@ export default function GAIA () {
             placeholder="Send a message"
             onChange={(e) => setMessage(e.target.value)}
           />
-          <Button 
-            placeholder="" 
+          <button 
             disabled={!message}
             className="!absolute right-0 top-0 p-6 rounded-lg bg-transparent" 
             onClick={(e) => chat(e, message)}
           >
             < FaArrowRight className={message ? "text-green-50 text-lg" : "text-green-50/40 text-lg"}/>
-          </Button>
+          </button>
         </form>
       </div>
     </main>
